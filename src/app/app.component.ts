19d0164314508459475a1363f4f5741276e42f89
Yigitcan Alanyali <y.alanyali@gmail.com>
2019-07-27 14:08:00 +0200

    CLEANUP: app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material';
-import { MomentDateAdapter, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loggedIn : boolean = false;
-
-  title = 'Termin App';
 }
